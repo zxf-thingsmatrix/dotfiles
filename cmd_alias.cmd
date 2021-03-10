aebf93280861473d1ce50d82246abcecf9cb76ad
zzz <zzz>
2021-03-10 21:25:51 +0800

    修改cmd_alias 中 link.vbs 路径

--- cmd_alias.cmd
+++ cmd_alias.cmd
@@ -3,10 +3,10 @@
 doskey ls=dir /p $*
 doskey ..=cd ..
 doskey cd=cd /d $*
-doskey link=link.vbs $*
+doskey link=%~dp0\link.vbs $*
 
-rem 设置 d:\qs 下所有 .lnk 的别名
-for %%i in (e:\zxf\qs\*.lnk) do (
+rem 设置 e:\qs 下所有 .lnk 的别名
+for %%i in (e:\qs\*.lnk) do (
 	doskey %%~ni=%%~nxi $*
 )
 
